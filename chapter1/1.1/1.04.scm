fa43db4fc09a1b0e3554a3b6d0f70d4b9a20a04f
lacunadream <d.s.yong@lse.ac.uk>
2017-03-17 12:04:46 +0800

    updated 1.04

--- chapter1/1.1/1.04.scm
+++ chapter1/1.1/1.04.scm
@@ -1,3 +1,3 @@
-uses the if condition to set the operator. 
+; uses the if condition to set the operator. 
 
-thus, if (> b 0) == true, (- a b) is executed which is equivalent to `${(a -- b)}` in js terms 
+; thus, if (> b 0) == true, (- a b) is executed which is equivalent to `${(a -- b)}` in js terms 
